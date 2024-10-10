a181c0511d6ac4da30f9da353029bf2211cd5d5f
Jorge Martinez <jorge.m@opendeusto.es>
2024-10-10 19:55:27 +0200

    Elementos añadidos

--- DeustoIkea/src/gui/VentanaMuebles.java
+++ DeustoIkea/src/gui/VentanaMuebles.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.util.ArrayList;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -23,9 +24,10 @@ public class VentanaMuebles extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	protected JButton botonCerrar, botonSeleccionar, botonComprar;
-	protected JPanel panelAbajo, panelCentro, panelCentroD, panelCentroI, panelArriba, panelImagen;
-	protected JLabel labelImagen, labelDescripcion;
+	protected JPanel panelAbajo, panelCentro, panelCentroD, panelCentroI, panelArriba, panelImagen, panelDescripcion;
+	protected JLabel labelImagen1, labelImagen2, labelImagen3, labelImagen4, labelDescripcion1, labelDescripcion2, labelDescripcion3, labelDescripcion4;
 	protected JTextArea areaTexto;
+	protected JTextField texto1, texto2, texto3, texto4;
 	protected String[] categoriasMuebles = { "Todos", "Silla", "Mesa", "Armario", "Sofá" };
 	protected JComboBox<String> comboDeMuebles;
 	protected ArrayList<Mueble> listaMuebles;
@@ -45,6 +47,11 @@ public class VentanaMuebles extends JFrame {
 		panelCentroI = new JPanel();
 		panelArriba = new JPanel(new BorderLayout());
 		panelImagen = new JPanel();
+		
+		texto1 = new JTextField(20);
+		texto2 = new JTextField(20);
+		texto3 = new JTextField(20);
+		texto4 = new JTextField(20);
 		 
         areaTexto = new JTextArea(3, 40);
         areaTexto.setLineWrap(true); // Permitir que el texto se ajuste a la línea
@@ -73,15 +80,36 @@ public class VentanaMuebles extends JFrame {
 		panelCentro.setLayout(new GridLayout(1, 2));
 		panelCentro.add(panelCentroI);
         panelCentro.add(panelCentroD);
+        panelCentroD.setLayout(new GridLayout(1, 2));
         
-//        panelCentroD.add(campoTexto);
+        panelCentroD.add(panelImagen);
+        panelCentroD.add(panelDescripcion);
         panelCentroI.add(comboDeMuebles);
         
-        labelImagen = new JLabel();
-        labelDescripcion = new JLabel();
+        ImageIcon sofa = new ImageIcon("src/Imagenes/sofa.jpeg");
+        ImageIcon armario = new ImageIcon("src/Imagenes/Armario.jpeg");
+        ImageIcon silla = new ImageIcon("src/Imagenes/silla.jpeg");
+        ImageIcon mesa = new ImageIcon("src/Imagenes/mesa.jpeg");
+        labelImagen1 = new JLabel(sofa);
+        labelImagen2 = new JLabel(armario);
+        labelImagen3 = new JLabel(silla);
+        labelImagen4 = new JLabel(mesa);
+        
+        
+        labelDescripcion1 = new JLabel();
+        labelDescripcion2 = new JLabel();
+        labelDescripcion3 = new JLabel();
+        labelDescripcion4 = new JLabel();
+        
         panelImagen.setLayout(new GridLayout(2, 1));
-        panelImagen.add(labelImagen);
-        panelImagen.add(labelDescripcion);
+        panelImagen.add(labelImagen1);
+        panelImagen.add(labelImagen2);
+        panelImagen.add(labelImagen3);
+        panelImagen.add(labelImagen4);
+        panelDescripcion.add(labelDescripcion1);
+        panelDescripcion.add(labelDescripcion2);
+        panelDescripcion.add(labelDescripcion3);
+        panelDescripcion.add(labelDescripcion4);
 	        
 		getContentPane().add(panelAbajo, BorderLayout.SOUTH);
 		getContentPane().add(panelCentro, BorderLayout.CENTER);
@@ -91,22 +119,22 @@ public class VentanaMuebles extends JFrame {
 			System.exit(0);
 		});
 				
-		botonSeleccionar.addActionListener((e) -> {
-            String categoriaSeleccionada = (String) comboDeMuebles.getSelectedItem();
-            mostrarMueblesSegunCategoria(categoriaSeleccionada);
-        });
+//		botonSeleccionar.addActionListener((e) -> {
+//            String categoriaSeleccionada = (String) comboDeMuebles.getSelectedItem();
+//            mostrarMueblesSegunCategoria(categoriaSeleccionada);
+//        });
 		
 		setVisible(true);
 		setLocationRelativeTo(null);
 	}
 	
-	private void mostrarMueblesSegunCategoria(String categoria) {
-        for (Mueble mueble : listaMuebles) {
-            if (categoria.equals("Todos") || mueble.getClass().getSimpleName().equals(categoria)) {
-                labelImagen.setIcon(mueble.getImagen());
-                labelDescripcion.setText(mueble.getDescripcion());
-                break;
-            }
-        }
-    }
+//	private void mostrarMueblesSegunCategoria(String categoria) {
+//        for (Mueble mueble : listaMuebles) {
+//            if (categoria.equals("Todos") || mueble.getClass().getSimpleName().equals(categoria)) {
+//                labelImagen.setIcon(mueble.getImagen());
+//                labelDescripcion.setText(mueble.getDescripcion());
+//                break;
+//            }
+//        }
+//    }
 }
