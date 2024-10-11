972ce70a636de7e209e54511a663d2b9979f8e05
Jorge Martinez <jorge.m@opendeusto.es>
2024-10-11 20:48:39 +0200

    Creacion de la ventana Inicio Sesion

--- DeustoIkea/src/gui/VentanaInicioSesion.java
+++ DeustoIkea/src/gui/VentanaInicioSesion.java
@@ -1,5 +1,18 @@
 package gui;
 
-public class VentanaInicioSesion {
+import javax.swing.*;
 
+public class VentanaInicioSesion extends JFrame {
+    private static final long serialVersionUID = 1L;
+
+    public VentanaInicioSesion() {
+        setTitle("Inicio de Sesión");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        setLocationRelativeTo(null); 
+        setVisible(true);
+    }
+
+    
 }
