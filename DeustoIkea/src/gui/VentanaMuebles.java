0d858bada1e5b14f51ab04a7419f96a8a57f1318
Jorge Martinez <jorge.m@opendeusto.es>
2024-10-11 18:06:54 +0200

    Cambios en la ventana de muebles

--- DeustoIkea/src/gui/VentanaMuebles.java
+++ DeustoIkea/src/gui/VentanaMuebles.java
@@ -2,11 +2,9 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -14,28 +12,20 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-import domain.Armario;
-import domain.Mesa;
-import domain.Mueble;
-import domain.Silla;
-import domain.Sofa;
 
 public class VentanaMuebles extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	protected JButton botonCerrar, botonSeleccionar, botonComprar;
-	protected JPanel panelAbajo, panelCentro, panelCentroD, panelCentroI, panelArriba, panelImagen, panelDescripcion;
+	protected JPanel panelAbajo, panelCentro, panelCentroD, panelCentroI, panelArriba;
 	protected JLabel labelImagen1, labelImagen2, labelImagen3, labelImagen4, labelDescripcion1, labelDescripcion2, labelDescripcion3, labelDescripcion4;
 	protected JTextArea areaTexto;
-	protected JTextField texto1, texto2, texto3, texto4;
-	protected String[] categoriasMuebles = { "Todos", "Silla", "Mesa", "Armario", "Sofá" };
-	protected JComboBox<String> comboDeMuebles;
-	protected ArrayList<Mueble> listaMuebles;
+//	protected ArrayList<Mueble> listaMuebles;
 	
 	public VentanaMuebles() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(500, 500);
 		setTitle("Muebles");
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		
 		botonCerrar = new JButton("CERRAR");
 		botonSeleccionar = new JButton("SELECCIONAR");
@@ -46,33 +36,18 @@ public class VentanaMuebles extends JFrame {
 		panelCentroD = new JPanel();
 		panelCentroI = new JPanel();
 		panelArriba = new JPanel(new BorderLayout());
-		panelImagen = new JPanel();
-		panelDescripcion = new JPanel();
 		
-		texto1 = new JTextField(20);
-		texto2 = new JTextField(20);
-		texto3 = new JTextField(20);
-		texto4 = new JTextField(20);
-		 
         areaTexto = new JTextArea(3, 40);
         areaTexto.setLineWrap(true); // Permitir que el texto se ajuste a la línea
         areaTexto.setWrapStyleWord(true); // Ajustar solo en palabras completas
         areaTexto.setEditable(false);
         areaTexto.setText("Bienvenido a DeustoIkea, tu tienda de muebles asequibles y modernos.\n" +
                           "Ofrecemos una amplia gama de muebles que se adaptan a todos los estilos y presupuestos.");
+        
 		
         JScrollPane scrollTexto = new JScrollPane(areaTexto);
         scrollTexto.setBorder(null);  // Remover borde
         panelArriba.add(scrollTexto, BorderLayout.NORTH); 
-        
-		comboDeMuebles = new JComboBox<>(categoriasMuebles);
-		
-		// Lista de muebles temp
-        listaMuebles = new ArrayList<>();
-        listaMuebles.add(new Silla());
-        listaMuebles.add(new Mesa());
-        listaMuebles.add(new Armario());
-        listaMuebles.add(new Sofa());
 		
 		panelAbajo.add(botonCerrar);
 		panelAbajo.add(botonSeleccionar);
@@ -81,11 +56,9 @@ public class VentanaMuebles extends JFrame {
 		panelCentro.setLayout(new GridLayout(1, 2));
 		panelCentro.add(panelCentroI);
         panelCentro.add(panelCentroD);
-        panelCentroD.setLayout(new GridLayout(1, 2));
         
-        panelCentroD.add(panelImagen);
-        panelCentroD.add(panelDescripcion);
-        panelCentroI.add(comboDeMuebles);
+        panelCentroI.setLayout(new GridLayout(2, 2));
+        panelCentroD.setLayout(new GridLayout(2, 2));
         
         ImageIcon sofa = new ImageIcon("src/Imagenes/sofa.jpeg");
         ImageIcon armario = new ImageIcon("src/Imagenes/Armario.jpeg");
@@ -101,21 +74,27 @@ public class VentanaMuebles extends JFrame {
         labelDescripcion2 = new JLabel();
         labelDescripcion3 = new JLabel();
         labelDescripcion4 = new JLabel();
+        labelDescripcion1.setText("<html><b>Sofá</b><br>Precio: $350<br>Peso: 40kg<br>Descripción: Sofá de tres plazas, cómodo y moderno, ideal para cualquier sala de estar.</html>");
+        labelDescripcion2.setText("<html><b>Armario</b><br>Precio: $200<br>Peso: 50kg<br>Descripción: Armario espacioso de dos puertas, con estantes internos para optimizar el almacenamiento.</html>");
+        labelDescripcion3.setText("<html><b>Silla</b><br>Precio: $75<br>Peso: 5kg<br>Descripción: Silla ergonómica con respaldo ajustable, perfecta para oficina o comedor.</html>");
+        labelDescripcion4.setText("<html><b>Mesa</b><br>Precio: $150<br>Peso: 20kg<br>Descripción: Mesa de comedor para seis personas, hecha de madera de alta calidad.</html>");
+//        labelDescripcion4.setText("Mesa \n" + "Precio: $150 \n" + "Peso: 20kg \n" + "Descripción: Mesa de comedor para seis personas, hecha de madera de alta calidad.");
         
-        panelImagen.setLayout(new GridLayout(2, 1));
-        panelImagen.add(labelImagen1);
-        panelImagen.add(labelImagen2);
-        panelImagen.add(labelImagen3);
-        panelImagen.add(labelImagen4);
-        panelDescripcion.add(labelDescripcion1);
-        panelDescripcion.add(labelDescripcion2);
-        panelDescripcion.add(labelDescripcion3);
-        panelDescripcion.add(labelDescripcion4);
+        panelCentroI.add(labelImagen1);
+        panelCentroI.add(labelDescripcion1);
+        panelCentroI.add(labelImagen2);
+        panelCentroI.add(labelDescripcion2);
+        
+        panelCentroD.add(labelImagen3);
+        panelCentroD.add(labelDescripcion3);
+        panelCentroD.add(labelImagen4);
+        panelCentroD.add(labelDescripcion4);
 	        
 		getContentPane().add(panelAbajo, BorderLayout.SOUTH);
 		getContentPane().add(panelCentro, BorderLayout.CENTER);
 		getContentPane().add(panelArriba, BorderLayout.NORTH);
-		
+          
+
 		botonCerrar.addActionListener((e) -> {
 			System.exit(0);
 		});
