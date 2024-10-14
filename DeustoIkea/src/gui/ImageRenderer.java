7e689bc18acda1505871ca1174a86be340721c5f
Jorge Martinez <jorge.m@opendeusto.es>
2024-10-14 18:21:25 +0200

    Cambios en los render

--- DeustoIkea/src/gui/ImageRenderer.java
+++ DeustoIkea/src/gui/ImageRenderer.java
@@ -8,13 +8,22 @@ public class ImageRenderer extends DefaultTableCellRenderer {
 	private static final long serialVersionUID = 1L;
 
 	@Override
-    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
-        if (value instanceof ImageIcon) {
-            JLabel label = new JLabel();
-            label.setIcon((ImageIcon) value);
-            return label;
-        } else {
-            return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-        }
-    }
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+	    JLabel label = new JLabel();
+	    if (value instanceof ImageIcon) {
+	        label.setIcon((ImageIcon) value);
+	        label.setHorizontalAlignment(JLabel.CENTER);
+	        label.setVerticalAlignment(JLabel.CENTER);
+	    } else {
+	        return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+	    }
+	    
+	    label.setPreferredSize(new java.awt.Dimension(100, 100));
+	    return label;
+	}
 }
+
+
+
+
+
