064a03ef522d7a76b1ac1f11c4cbae5c3d375b31
Jorge Martinez Garcia <jorge.m@opendeusto.es>
2024-11-20 17:54:20 +0100

    Tabuladores añadidos

--- DeustoIkea/src/gui/ModeloCarrito.java
+++ DeustoIkea/src/gui/ModeloCarrito.java
@@ -13,9 +13,9 @@ public class ModeloCarrito extends DefaultTableModel{
 	private List<Producto> lProductos;
 
 	public ModeloCarrito(List<Producto> lProductos) {
-		this.lProductos = lProductos;
-		
+		this.lProductos = lProductos;	
 	}
+	
 	@Override
 	public int getColumnCount() {
 		return titulos.size();
@@ -28,37 +28,39 @@ public class ModeloCarrito extends DefaultTableModel{
 
 	@Override
 	public int getRowCount() {
-		if(lProductos == null)
+		if(lProductos == null) {
 			return 0;
-		return lProductos.size();
+		} else {
+			return lProductos.size();
+		}
 	}
+	
 	@Override
 	public boolean isCellEditable(int row, int column) {
 		return false;
 	}
 	
-	
-	
 	@Override
 	public Object getValueAt(int row, int column) {
 		Producto p = lProductos.get(row);
 		switch(column) {
-	
-		case 0: return p.getClass().getSimpleName();
-		case 1: return p.getNumeroProductos();
-		case 2:return p.getPrecio()*p.getNumeroProductos();
-		default: return null;
+			case 0: return p.getClass().getSimpleName();
+			case 1: return p.getNumeroProductos();
+			case 2:return p.getPrecio()*p.getNumeroProductos();
+			default: return null;
 		}
 	}
+	
 	 public void eliminarProducto(Producto producto) {
-	        lProductos.remove(producto);
-	        fireTableDataChanged(); 
-	    }
-	    public void actualizarTabla() {
-	        fireTableDataChanged(); 
-	    }
+        lProductos.remove(producto);
+        fireTableDataChanged(); 
+    }
+	 
+    public void actualizarTabla() {
+        fireTableDataChanged(); 
+    }
 
-	    public Producto getProductoAtRow(int row) {
-	        return lProductos.get(row);
-	    }
+    public Producto getProductoAtRow(int row) {
+        return lProductos.get(row);
+    }
 }
